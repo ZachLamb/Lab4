2cfc611edf14c1723efae91c76875b0cc35a5b81
ZachLamb <ZachLamb@users.noreply.github.com>
2014-09-17 15:24:11 -0600

    comment

--- src/lab4/Hello.java
+++ src/lab4/Hello.java
@@ -3,7 +3,7 @@ package lab4;
 public class Hello {
 	public static void main(String[] args){
 		System.out.println("Hello World"); //Display the string
-		System.out.println("How are you?");
+		System.out.println("How are you?");//Display the string
 	}
 
 }
