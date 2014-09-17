2bec589d36ac8ef0fcc52839c9f72a198b4e8d4e
ZachLamb <ZachLamb@users.noreply.github.com>
2014-09-17 15:48:22 -0600

    new comment

--- src/lab4/Hello.java
+++ src/lab4/Hello.java
@@ -4,6 +4,7 @@ public class Hello {
 	public static void main(String[] args){
 		System.out.println("Hello World"); //Display the string
 		System.out.println("How are you?");//Display the string
+		//new comment
 	}
 
 }
