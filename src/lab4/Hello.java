9589d4342ee2ecb6d33cfd82c9d20fde5e32971a
ZachLamb <zala3440@colorado.edu>
2014-09-17 16:03:38 -0600

    commit

--- src/lab4/Hello.java
+++ src/lab4/Hello.java
@@ -5,6 +5,7 @@ public class Hello {
 		System.out.println("Hello World"); //Display the string
 		System.out.println("How are you?");//Display the string
 		//new comment
+		//another comment
 	}
 
 }
